1b8b046ffc761e81749e2aabd0e2f46db2674f89
Victor <matheus0588@outlook.com>
2022-06-21 15:01:11 -0300

    Feito requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class App extends React.Component {
       <div>
         <p>Sistema Solar</p>
         <Header />
+        <SolarSystem />
       </div>
     );
   }
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="solar-system">Sistema Solar</h1>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
