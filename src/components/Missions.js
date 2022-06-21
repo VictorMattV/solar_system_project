02db923b03651350c4fd7eda492c25bcd11f9561
Victor <matheus0588@outlook.com>
2022-06-21 18:29:15 -0300

    Feito requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions"> </div>
+      <div>
+        <Title headline="Missões" />
+        <div data-testid="missions" />
+      </div>
     );
   }
 }
