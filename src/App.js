44a88cc7302a729f4e2b853a5914174f42db91ab
Victor <matheus0588@outlook.com>
2022-06-21 18:20:37 -0300

    Feito requisito 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Missions />
         <Title headline="" />
       </div>
     );
