bc79e6862c141255b5bedafef5a26e43579e7c48
Victor <matheus0588@outlook.com>
2022-06-21 21:25:07 -0300

    Corrigido erros em requisitos anteriores

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <>
+      <div>
+        <Title headline="Missões" />
+        <div data-testid="missions" />
         {
           missions.map((mission) => (
             <MissionCard
@@ -17,7 +20,7 @@ class Missions extends React.Component {
             />
           ))
         }
-      </>
+      </div>
     );
   }
 }
